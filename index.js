443740e5be6bb36a30d21bf16ac5f1c5888ecd99
KIMINJOONG <ijboym216@gmail.com>
2019-01-29 21:35:41 +0900

    노드 서버세팅

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hi!");
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+
+// app에게 4000포트를 리슨하라고 한다.
+// 리스닝을 시작할때 handleListening함수를 실행한다
+app.listen(PORT, handleListening);
