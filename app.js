5ca69a872a8b4ab21ad149bbd117a1a01ac031c4
KIMINJOONG <ijboym216@gmail.com>
2019-02-02 06:34:58 +0900

    전역변수를 추가하는 방법

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -16,12 +17,17 @@ const app = express();
 // 접속이 있을때 위에서부터 아래로 실행됨
 // 원하는 만큼 middleware를 선언후 route를 실행
 //
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})); 
-app.use(helmet());
 app.use(morgan("dev"));
+
+
+
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
