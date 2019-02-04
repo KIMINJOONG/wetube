81065f1b807be3bfe999db2c01f079b316a7df48
KIMINJOONG <ijboym216@gmail.com>
2019-02-04 09:10:17 +0900

    model만들고 연동해주기

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,16 @@
-import { videos } from "../db";
 import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req, res) => {
-    res.render("home", { pageTitle : "Home", videos});
+
+export const home = async(req, res) => {
+    //database에 모든 video를 갖고오라는 의미
+    try{
+        const videos = await Video.find({}); 
+        res.render("home", { pageTitle : "Home", videos});
+    }catch(error) {
+        console.log(error);
+        res.render("home", { pageTitle : "Home", videos: []});
+    }
 }
 export const search = (req, res) => {
     const { 
--- db.js
+++ db.js
@@ -1,50 +1,18 @@
-export const videos = [
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL, 
     {
-        id: 324393,
-        title: "Video awesome",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https:/archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Kim In Joong",
-            email: "ijboym216@gmail.com"
-        }
-    },
-    {
-        id: 1231231,
-        title: "Video super",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https:/archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Kim In Joong",
-            email: "ijboym216@gmail.com"
-        }
-    },
-    {
-        id: 5555555,
-        title: "Video nice",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https:/archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Kim In Joong",
-            email: "ijboym216@gmail.com"
-        }
-    },
-    {
-        id: 111111,
-        title: "Video perfect",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https:/archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-        creator: {
-            id: 121212,
-            name: "Kim In Joong",
-            email: "ijboym216@gmail.com"
-        }
-    }
-]
+        useNewUrlParser: true,
+        useFindAndModify: false
+    });
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB");
+const handleError = (error) => console.log(`Error on DB Connection : ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
