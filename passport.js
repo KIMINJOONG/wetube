609fb5c04bc02d5de281b996a04be52daab497e1
KIMINJOONG <ijboym216@gmail.com>
2019-02-09 11:26:58 +0900

    회원가입 작업중 컨트롤러부분 진행중

--- passport.js
+++ passport.js
@@ -2,3 +2,9 @@ import passport from "passport";
 import User from "./models/User";
 
 passport.use(User.createStratgey());
+
+// 여 passport야 쿠키에는 오직 user.id만 담아서 보내도록해 라고 말해주는것과 같음
+passport.serializeUser(User.serializeUser());
+// 일반적으로 사람들은 쿠키에 user.id를 넣고
+// 그 다음 그 id로 사용자를 식별함
+passport.deserializeUser(User.deserializeUser());
