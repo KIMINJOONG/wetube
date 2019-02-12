9fe2f7901a3e6578104090ee858202070eb4dd30
KIMINJOONG <ijboym216@gmail.com>
2019-02-12 22:10:53 +0900

    동영상 재생시간, 현재재생시간 구현

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -3,6 +3,8 @@ let videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
 const volumeBtn = document.getElementById("jsVolumeBtn");
 const fullScreenBtn = document.getElementById("jsFullScreen");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
 function handlePlayClick() {
     if(videoPlayer.paused) {
@@ -54,10 +56,42 @@ function goFullScreen() {
     fullScreenBtn.addEventListener("click", exitFullScreen);
 }
 
+
+const formatDate = seconds => {
+    const secondsNumber = parseInt(seconds, 10);
+    let hours = Math.floor(secondsNumber / 3600);
+    let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
+    let totalSeconds = secondsNumber - hours * 3600 - minutes * 60;
+
+    if(hours < 10) {
+        hours = `0${hours}`;
+    }
+    if(minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    if(seconds < 10) {
+        totalSeconds = `0${totalSeconds}`;
+    }
+    return `${hours}:${minutes}:${totalSeconds}`;
+};
+
+function getCurrentTime() {
+    currentTime.innerHTML = formatDate(videoPlayer.currentTime); 
+}
+
+function setTotalTime() {
+    const totalTimeString = formatDate(videoPlayer.duration);
+    totalTime.innerHTML = totalTimeString;
+    setInterval(getCurrentTime, 1000);
+}
+
+
 function init() {
     playBtn.addEventListener("click", handlePlayClick);
     volumeBtn.addEventListener("click", handleVolumeClick);
     fullScreenBtn.addEventListener("click", goFullScreen);
+    // video가 metadata를 로드할때까지 기다렸다가 setTotalTime함수를 실행
+    videoPlayer.addEventListener("loadedmetadata", setTotalTime);
 }
 
 if(videoContainer) {
