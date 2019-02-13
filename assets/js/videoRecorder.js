671d9bf6cd45304dddfd65429f971f2dfc1de625
KIMINJOONG <ijboym216@gmail.com>
2019-02-13 22:10:32 +0900

    비디오 녹화, 저장 기능 구현

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,7 +2,35 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
-const startRecording = async() => {
+let streamObject;
+let videoRecorder;
+
+const handleVideoData = (event) => {
+    const { data: videoFile } = event;
+    const link = document.createElement("a");
+    link.href= URL.createObjectURL(videoFile);
+    link.download = "recorded.webm";// 오픈소스이기때문에 webm사용
+    document.body.appendChild(link);
+    link.click();
+};
+
+const stopRecording = () => {
+    videoRecorder.stop();
+    recordBtn.removeEventListener("click", stopRecording);
+    recordBtn.addEventListener("click", getVideo);
+    recordBtn.innerHTML = "Start Recording";
+};
+
+const startRecording = () => {
+    videoRecorder = new MediaRecorder(streamObject);
+    videoRecorder.start();
+    videoRecorder.addEventListener("dataavailable", handleVideoData);
+    recordBtn.addEventListener("click", stopRecording);
+};
+
+
+
+const getVideo = async() => {
     try{
         // await하는 이유? 유저가 결정할때까지 기다림
         // 우리가 media에 접근 할 수 있게 해줄지 아닐지
@@ -13,14 +41,18 @@ const startRecording = async() => {
         videoPreview.srcObject = stream;
         videoPreview.muted = true;
         videoPreview.play();
+        recordBtn.innerHTML = "Stop recording";
+        streamObject = stream;
+        startRecording();
     }catch(error) {
         recordBtn.innerHTML = ":( Cant record";
-        recordBtn.removeEventListener("click", startRecording);
+    } finally {
+        recordBtn.removeEventListener("click", getVideo);
     }
 }
 
 function init() {
-    recordBtn.addEventListener("click", startRecording);
+    recordBtn.addEventListener("click", getVideo);
 }
 
 if(recorderContainer) {
