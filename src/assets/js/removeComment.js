6a5a7cf028efbfd5400ed341c7f496633213954a
KIMINJOONG <ijboym216@gmail.com>
2019-03-01 19:49:59 +0900

    fixed delte comment function

--- src/assets/js/removeComment.js
+++ src/assets/js/removeComment.js
@@ -1,28 +1,27 @@
 import axios from "axios";
-const removeCommentBtn = document.querySelectorAll(".removeCommentBtn");
+const removeCommentUl = document.getElementById("jsCommentList");
 
 const removeComment = async(event) => {
-    const videoId = window.location.href.split("/videos/")[1];
-    const commentId = event.target.value;
-    const response = await axios({
-        url: `/api/${videoId}/removeComment`,
-        method: "POST",
-        data: {
-            commentId
+    if(event.srcElement.tagName === "BUTTON") {
+        const videoId = window.location.href.split("/videos/")[1];
+        const commentId = event.target.value;
+        const response = await axios({
+            url: `/api/${videoId}/removeComment`,
+            method: "POST",
+            data: {
+                commentId
+            }
+        });
+        if(response === 200) {
+            console.log("전달 성공");
         }
-    });
-    if(response === 200) {
-        console.log("전달 성공");
     }
 };
 
 function init() {
-    //removeCommentBtn.addEventListener("click", removeComment);
-    for(let i = 0; i < removeCommentBtn.length; i++) {
-        removeCommentBtn[i].addEventListener("click", removeComment);
-    }
+    removeCommentUl.addEventListener("click", removeComment);
 }
 
-if(removeCommentBtn) {
+if(removeCommentUl) {
     init();
 }
